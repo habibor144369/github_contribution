45a4c4b4428e935f6438b767bd7348edb6792f61
habibor144369 <onilhabib1@gmail.com>
2022-11-29 14:02:56 +0600

    ok

---
+++ data.js
@@ -0,0 +1,17 @@
+const data = [
+    {
+        name: 'habibor Rahaman',
+        age: 22,
+        email: 'habibor.rahaman1010@gmail.com'
+    },
+    {
+        name: 'habibor Rahaman',
+        age: 22,
+        email: 'habibor.rahaman1010@gmail.com'
+    },
+    {
+        name: 'habibor Rahaman',
+        age: 22,
+        email: 'habibor.rahaman1010@gmail.com'
+    },
+]
